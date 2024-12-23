9df528e2df67b9eeeb2aa4c77d6bb00692e8454d
Gio <9650600+its-gio@users.noreply.github.com>
2024-12-23 10:55:48 -0800

    Adding page and fetch for startup by id

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -7,7 +7,6 @@ export const STARTUP_QUERY = defineQuery(
       _id,
       title,
       slug,
-      pitch,
       category,
       image,
       description,
@@ -17,6 +16,27 @@ export const STARTUP_QUERY = defineQuery(
         _id,
         bio,
         email,
+        name,
+        username,
+        image
+      }
+  }`,
+);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(
+  `*[_type == "startup" && _id == $id][0] {
+      _id,
+      title,
+      slug,
+      pitch,
+      category,
+      image,
+      description,
+      views,
+      _createdAt,
+      author -> {
+        _id,
+        bio,
         name,
         username,
         image
