022960406c9f0ea983272529787a0088a4033a38
Gio <9650600+its-gio@users.noreply.github.com>
2025-08-05 20:34:46 -0700

    Adding nav mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -12,11 +14,12 @@ const Navbar = async () => {
           <Image src="/logo.png" alt="Stock YC Logo" width={144} height={30} />
         </Link>
 
-        <div className="flex item-center gap-5">
+        <div className="flex items-center gap-5">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
               <form
@@ -26,11 +29,21 @@ const Navbar = async () => {
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">Logout</button>
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
               </form>
 
               <Link href={`/user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
+                <span className="max-sm:hidden">{session?.user?.name}</span>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={"Your image silly!"}
+                  />
+                  <AvatarFallback>Missing Avatar</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
